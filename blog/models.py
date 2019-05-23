7f9d6d99362cd8453b6730ab0953bf041bda21f0
porwal.mayank075@gmail.com <Mynk>
2019-05-23 18:57:08 +0530

    final

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create a blog models
+class Blog(models.Model):
+	title = models.CharField(max_length=255)
+	pub_date = models.DateTimeField()
+	image = models.ImageField(upload_to='images/')
+	body = models.TextField()
+
+	def __str__(self):
+			return self.title
+				
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
+
+
+# Add to admin
