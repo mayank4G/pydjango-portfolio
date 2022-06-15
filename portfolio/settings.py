9e85bcb346f0213f36bdf1068db9cb3f89752f00
mayank <porwal.mayank075@gmail.com>
2022-06-15 11:49:45 +0530

    updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'portfoiliodb',
         'USER' : 'postgres',
         'PASSWORD': 'django',
         'HOST' : 'localhost',
-        'PORT' : '5432'
+        'PORT' : '8000'
         }
 }
 
